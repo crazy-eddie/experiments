922464d1e94d5d87eef3e2a448cf7b479e5546f2
Noah Roberts <noah.roberts@hushmail.com>
2014-04-03 23:10:24 -0700

    Fundamental types.

---
+++ functional/inc/core/fundamentals.hpp
@@ -0,0 +1,87 @@
+#ifndef FUNCTIONAL_CORE_FUNDAMENTALS_HPP
+#define FUNCTIONAL_CORE_FUNDAMENTALS_HPP
+
+#include "curry.hpp"
+
+namespace functional { namespace core {
+
+
+template < typename RawType >
+struct raw_wrapper
+{
+	raw_wrapper(RawType v) : val(v) {}
+
+	template < typename T >
+	explicit raw_wrapper(raw_wrapper<T> const& other)
+		: val(other.raw_value())
+	{}
+
+	RawType raw_value() const { return val; }
+
+	operator RawType () const { return raw_value(); }
+
+	raw_wrapper value() const { return *this; }
+	raw_wrapper operator() () const { return value(); }
+
+private:
+	RawType val;
+};
+
+template < typename T >
+bool eq(raw_wrapper<T> const& lh, raw_wrapper<T> const& rh)
+{
+	return lh.raw_value() == rh.raw_value();
+}
+// variadic eq?
+
+template < typename T >
+bool operator == (raw_wrapper<T> const& lh, raw_wrapper<T> const& rh)
+{
+	return lh.raw_value() == rh.raw_value();
+}
+
+#define RAW_VALUE_OPERATOR(sym, op) \
+	struct sym ## __{ \
+		template < typename T0, typename T1 > \
+		auto operator() (raw_wrapper<T0> const& a0, raw_wrapper<T1> const& a1) const \
+		{ \
+			using rtype = decltype(std::declval<T0>() op std::declval<T1>()); \
+			return raw_wrapper<rtype>(a0.raw_value() op a1.raw_value()); \
+		} \
+		template < typename T0, typename T1, typename ... T > \
+		auto operator() (raw_wrapper<T0> const& a0, raw_wrapper<T1> const& a1, raw_wrapper<T> const& ... a) const \
+		{ \
+			return (*this)((*this)(a0,a1), a...); \
+		} \
+	} sym ## _; \
+	template < typename T0, typename T1 > \
+	auto operator op (raw_wrapper<T0> const& a0, raw_wrapper<T1> const& a1) \
+	{ \
+		return sym ## _(a0,a1); \
+	} \
+	auto sym = curry(sym ## _)
+
+RAW_VALUE_OPERATOR(sum, +);
+RAW_VALUE_OPERATOR(div, /);
+RAW_VALUE_OPERATOR(minus, -);
+RAW_VALUE_OPERATOR(multiply, *);
+RAW_VALUE_OPERATOR(land, &&);
+RAW_VALUE_OPERATOR(band, &);
+RAW_VALUE_OPERATOR(lor, ||);
+RAW_VALUE_OPERATOR(bor, |);
+RAW_VALUE_OPERATOR(bxor, ^);
+
+template < typename T >
+std::ostream& operator << (std::ostream & out, raw_wrapper<T> const& val)
+{
+	return out << val.raw_value();
+}
+
+using int_ = raw_wrapper<int>;
+using double_ = raw_wrapper<double>;
+using char_ = raw_wrapper<char>;
+
+
+}}
+
+#endif
---
+++ functional/test/fundamentals.cpp
@@ -0,0 +1,39 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE Fundamentals
+#include <boost/test/unit_test.hpp>
+
+#include "../inc/core/fundamentals.hpp"
+
+// All types are also functions that return themselves.
+// On top of that functions have a value() member that evaluates their result.
+
+namespace fn = functional::core;
+
+
+BOOST_AUTO_TEST_CASE(construction)
+{
+	fn::int_ i = 5;
+
+	BOOST_CHECK_EQUAL(i, 5); // checks both that == works and that
+}
+
+BOOST_AUTO_TEST_CASE(math)
+{
+	fn::int_ i0 = 5;
+	fn::int_ i1 = 2;
+	fn::int_ i2 = 6;
+
+	BOOST_CHECK_EQUAL((i0 + i1).value(), 7);
+	BOOST_CHECK_EQUAL(fn::sum(i0,i1,i2).value(), 13);
+}
+
+BOOST_AUTO_TEST_CASE(mixed)
+{
+	fn::int_ i = 5;
+	fn::char_ c = 'c' - 'a';
+	fn::double_ d = 6.7;
+
+	fn::int_ result = static_cast<fn::int_>(i + c + d);
+
+	BOOST_CHECK_EQUAL(result, 13);
+}
