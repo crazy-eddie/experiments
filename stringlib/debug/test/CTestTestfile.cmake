f0577c5a5c1ce09c57fab6c37f934e7f59c4152a
Noah Roberts <noah.roberts@hushmail.com>
2016-03-08 12:15:19 -0800

    Remove build directories

--- stringlib/debug/test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/satan/workspace/strings/test
-# Build directory: /home/satan/workspace/strings/debug/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- trie/debug/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/satan/workspace/trie/test
-# Build directory: /home/satan/workspace/trie/debug/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(runtime_trie "./runtime_trie")
